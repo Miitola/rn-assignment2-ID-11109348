c0fea00f132965b959e00343de72ec61146ff2ef
hayat hani-tikass <hayathani-tikass@hayats-MacBook-Air.local>
2024-05-27 10:32:41 +0000

    background color

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#e0f2f1',
     alignItems: 'center',
     justifyContent: 'center',
   },
